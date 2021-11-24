0b607dd4add51cc6388800da1f7b5e9519f174ed
4104W019 <4104W019@mail.ntou.edu.tw>
2021-11-24 17:52:54 +0800

    HW5 add system level domain characteristics testing sample code

--- Homework/HW5/inputdomain.h
+++ Homework/HW5/inputdomain.h
@@ -2,6 +2,7 @@
 #define INPUTDOMAIN_H
 #include <stdint.h>
 #include <QString>
+#include <QList>
 /**
  * Input Domain Characterization
 (Testing-Introduction.ppt p.65:
@@ -30,8 +31,20 @@ public:
         MATH_PointOutOfRange,
         MATH_Failed,
         MATH_Pass,
+        LANG_MISSED_ONE,
+        LANG_MISSED_TWO,
+        LANG_MISSED_ALL,
+        LANG_OVER_THREE,
+        MATH_MISSED_ONE,
+        MATH_MISSED_TWO,
+        MATH_MISSED_ALL,
+        MATH_OVER_THREE,
         SubjectOutOfRange
     };
+
+
+    //( <=0, 1, >1 )
+
     /**
      * @brief domainOfExams
      * @param subject: 0, 1;
@@ -39,6 +52,14 @@ public:
      * @return
      */
     int32_t domainOfExams(int32_t subject, int32_t point);
+
+    /**
+     * @brief Used to measure the grade.
+     * @param subject: 0, 1:
+     * @param points[IN]: a list of grade point
+     * @return
+     */
+    uint32_t finalGrade(int32_t subject, QList<uint8_t> &points);
 };
 
 #endif // INPUTDOMAIN_H
